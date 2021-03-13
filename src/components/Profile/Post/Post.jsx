9951e906f1e46a6f5cc0b6aa9892339988c1206b
Sergey <Sergey.rimsha.ru@gmail.com>
2021-03-13 23:27:02 +0300

    lesson work 16

--- src/components/Profile/Post/Post.jsx
+++ src/components/Profile/Post/Post.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import style from './Post.module.css';
 
 const Post = () => {
+    let count = 0;
+    console.log(count);
     return (
         <div className={style.post}>
             <img src="" alt='post user photo'></img>
             <div className={style.postTxt}>Post_1</div>
+            <button onClick={() => {
+                console.log(++count);
+            }}>like</button>
         </div>
     )
 }
