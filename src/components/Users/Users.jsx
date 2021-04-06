e22815928e4a76129dca0eebf4c080e0e0ef38ad
Sergey <Sergey.rimsha.ru@gmail.com>
2021-04-06 12:47:04 +0300

    lesson work 54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,14 +3,12 @@ import style from './Users.module.css';
 import * as axios from 'axios';
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
 
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
-        
     }
 
     render() {
