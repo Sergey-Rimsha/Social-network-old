7e66fb85b34591e3199cf7da2c588a522ae684d3
Sergey <Sergey.rimsha.ru@gmail.com>
2021-05-03 20:58:05 +0300

    lesson work 71 setState

--- src/components/Profile/ProfileInfoUsers/ProfileUsers.jsx
+++ src/components/Profile/ProfileInfoUsers/ProfileUsers.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import style from './../Profile.module.css';
 import userPhoto from './../../../assets/images/ava_default.jpg';
 import Preloader from './../../../components/common/Preloader/Preloader';
+import ProfileStatus from '../ProfileStatus/ProfileStatus';
 
 
 const ProfileUsersWrap = (props) => {
@@ -17,6 +18,7 @@ const ProfileUsersWrap = (props) => {
                 </div>
                 <div className={style.userInfo}>
                     <div>{props.profile.fullName}</div>
+                    <ProfileStatus status={'hello world!!'}/>
                     <div>Date of Britch: <span>7.08.1994</span></div>
                     <div>Education: <span>БГПК</span></div>
                     <div>Web Site: <span>Vk.com</span></div>
