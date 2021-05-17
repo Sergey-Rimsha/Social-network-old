51e61b15d13612f62416e97e4cb37477c93832a3
Sergey <Sergey.rimsha.ru@gmail.com>
2021-05-17 21:00:41 +0300

    94: Suspense, lazy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, lazy, Suspense } from 'react';
 import {Route, withRouter} from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import UsersContainer from './components/Users/UsersContainer';
 
@@ -13,6 +12,10 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import {initializeApp} from './redux/appReducer';
 import Preloader from './components/common/Preloader/Preloader';
+import { withSuspense } from './hoc/withSuspense';
+
+
+const Profile = lazy(() => import('./components/Profile/Profile'));
 
 class App extends Component {
 
@@ -30,11 +33,11 @@ class App extends Component {
                 <div className="grid">
                     <HeaderContainer />
                     <Navbar />
-                    <div className='content'>
-                        <Route path={`/profile/:userId?`} component={Profile} />
-                        <Route path={`/dialogs`} component={DialogsContainer} />
-                        <Route path={`/users`} component={UsersContainer} />
-                        <Route path={`/login`} component={Login} />
+                    <div className='content'>                          
+                            <Route path={`/profile/:userId?`} component={withSuspense(Profile)} />    
+                            <Route path={`/dialogs`} component={withSuspense(DialogsContainer)} />
+                            <Route path={`/users`} component={withSuspense(UsersContainer)} />
+                            <Route path={`/login`} component={Login} />
                     </div>
                 </div>
             </div>
