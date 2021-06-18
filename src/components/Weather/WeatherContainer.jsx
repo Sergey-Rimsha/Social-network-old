bd13aa759f726b5a23860e56c740ea6e5fa36937
Sergey <Sergey.rimsha.ru@gmail.com>
2021-06-18 12:53:04 +0300

    weather_6

--- src/components/Weather/WeatherContainer.jsx
+++ src/components/Weather/WeatherContainer.jsx
@@ -6,67 +6,75 @@ import {setWeatherThunk} from './../../redux/weatherReducer';
 import Preloader from '../common/Preloader/Preloader';
 
 
-class WeatherContainer extends React.Component {
-    componentDidMount() {
-        this.props.setWeatherThunk()
-        console.log(this.props)
-        console.log('componentDidMount')
-        console.log(this.props.name)
+// class WeatherContainer extends React.Component {
+//     componentDidMount() {
+//         this.props.setWeatherThunk()
+//         console.log(this.props)
+//         console.log('componentDidMount')
+//         console.log(this.props.name)
         
-    }
-
-
-    render() {
-        return (
-            <div>
-                Weather City:
-                {this.props.name && "city"}
-                <button >getWeather</button>
-            </div>
-        )
-    }
-}
-
-// const WeatherContainer = (props) => {
-
-//     // let [weather, setWeather] = useState('props.weather')
-//     // let [nameCity, setNameCity] = useState("props.weather.name")
-
-//     // useEffect(() => {
-//     //     console.log('render')
-//     //     props.setWeatherThunk()
-//     //     // setWeather({...props.weather})
-        
-//     // }, []);
-
-//     // useEffect(() => {
-//     //     // setNameCity(props.weather.name)
-//     //     console.log('reloaded_useEffect')
-//     // }, [props])
-
-//     const weatherApi = () => {
-//         // setWeather({...props.weather})
-//         props.setWeatherThunk()
-//         console.log(props.weather)
-//         // console.log(nameCity)
-//         // setNameCity(props.weather.name)
-//         console.log('get API')
 //     }
 
-//     if (!props.weather) {
-//         return (
-//             <Preloader />
-//         )
-//     } 
+//     componentDidUpdate() {
+//         if (this.props.weather === null) {
+//             return <Preloader />
+//         } else {
 
-//     return (
-//         <div>
-//             Weather City:
-//             {props.weather.name || "city"}
-//             <button onClick={() => weatherApi()} >getWeather</button>
-//         </div>
-//     )
+//         }
+//     }
+
+
+//     render() {
+//         return (
+//             <div>
+//                 Weather City:
+//                 {this.props.name && "city"}
+//                 <button >getWeather</button>
+//             </div>
+//         )
+//     }
 // }
+
+const WeatherContainer = (props) => {
+
+    const [weather, setWeather] = useState("props.weather");
+    const [nameCity, setNameCity] = useState("props.weather.name");
+    const [temp, setTemp] = useState("props.weather.main.temp");
+
+    useEffect(() => {
+        console.log('render')
+        props.setWeatherThunk()     
+    }, []);
+
+    useEffect(() => {
+        try {
+            setNameCity(props.weather.name);
+            setTemp(props.weather.main.temp);
+            setWeather(props.weather);
+            console.log('try') 
+        } catch {
+            console.log('catch')
+        }
+        console.log('reloaded_useEffect')
+    }, [props])
+
+    if (!props.weather) {
+        return (
+            <Preloader />
+        )
+    }
+    return (
+        <div>
+            <span>Weather City: </span>
+            {nameCity || "city"}
+            <div>
+                <span>Temp: </span>
+                {temp}
+            </div>
+            <button>getWeather</button>
+        </div>
+    )
+}
 
 const mapStateToProps = (state) => ({
     weather: state.weather.weather
